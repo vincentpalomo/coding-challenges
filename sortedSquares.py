54287342d300c0b163cfe764d237f6eaa1e15bc5
vincent <vincentpalomo@yahoo.com>
2023-07-27 21:24:19 -0500

    solved sorted square with more efficient solution

--- sortedSquares.py
+++ sortedSquares.py
@@ -1,4 +1,4 @@
-
+# O(n log n) time | O(n) space
 def sortedSquaredArrayBrute(array):
     # create an empty array with the length of input array filled with 0
     sortedSqaures = [0 for _ in array]
@@ -15,4 +15,35 @@ def sortedSquaredArrayBrute(array):
 
 
 ret = sortedSquaredArrayBrute([-2, 1, 3, 9, 6])
+print(ret)
+
+
+def sortedSquaredArray(array):
+    # create an empty array with lenght of input array with 0
+    sortedSquares = [0 for _ in array]
+
+    # init two pointers, start at beginning of array and end at the end
+    start = 0
+    end = len(array) - 1
+
+    # traverse the array from the end to the beginning
+    for i in reversed(range(len(array))):
+        # get values of elements at start and end
+        small = array[start]
+        large = array[end]
+
+        # compare the absolute values and square the larger one
+        if abs(small) > abs(large):
+            sortedSquares[i] = small * small
+            # move start pointer to the right
+            start += 1
+        else:
+            sortedSquares[i] = large * large
+            # move end pointer to the left
+            end -= 1
+
+    return sortedSquares
+
+
+ret = sortedSquaredArray([-4, 1, 2, 5, 9, 10])
 print(ret)
