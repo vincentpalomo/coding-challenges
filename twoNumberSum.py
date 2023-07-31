364b056b93ac69c1a10478fc3e80948d3867f1d5
Vincent Palomo <vincentpalomo@yahoo.com>
2023-07-30 21:44:52 -0500

    updated print for readability

--- twoNumberSum.py
+++ twoNumberSum.py
@@ -1,27 +1,32 @@
 def twoNumberSum(array, target):
-  array.sort()
+    array.sort()
 
-  left = 0
-  right = len(array) - 1
+    left = 0
+    right = len(array) - 1
 
-  while left < right:
-    currentSum = array[left] + array[right]
-    if currentSum == target:
-      print('Answer: ', array[left], array[right])
-      return [array[left], array[right]]
-    elif currentSum < target:
-      left += 1
-    elif currentSum > target:
-      right -= 1
+    while left < right:
+        currentSum = array[left] + array[right]
+        if currentSum == target:
+            print('Answer: ', array[left], array[right])
+            return [array[left], array[right]]
+        elif currentSum < target:
+            left += 1
+        elif currentSum > target:
+            right -= 1
+
 
 twoNumberSum([1, 3, -4, -5, 10], 13)
 
-def twoNumberSumHash(array, target):
-  nums = {}
-  for num in array:
-    if target - num in nums:
-      return [target - num, num]
-    else:
-      nums[num] = True
 
-twoNumberSumHash([1, 3, -4, -5, 10], 13)
+def twoNumberSumHash(array, target):
+    nums = {}
+    for num in array:
+        if target - num in nums:
+            return [target - num, num]
+        else:
+            nums[num] = True
+    return []
+
+
+ret = twoNumberSumHash([1, 3, -4, -5, 10], 13)
+print('Hash return', ret)
